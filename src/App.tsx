af84cea41808696e403b1299642e628a41a68e5e
itzsebas121 <sebastipan121@gmail.com>
2025-05-07 00:17:58 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FacebookLogin from './pages/Facebook/LoginFacebook'
+
+function App() {
+
+  return (
+    <>
+      <FacebookLogin></FacebookLogin>
+    </>
+  )
+}
+
+export default App
